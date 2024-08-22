5aaf4fe5bf7f7afbc7b9e98784f697fb2effc361
Suresh <sureshyadav4032@gmail.com>
2024-08-22 12:04:09 +0530

    Add files via upload

---
+++ new project/client/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import Compaines from './components/Compaines/Compaines';
+import Politician from './components/Politician/Politician';
+import GetStarted from './components/GetStarted/GetStarted';
+import Footer from './components/Footer/Footer';
+import Contact from './components/Contact/Contact';
+import Value from './components/Value/Value';
+import SocialMedia from './components/SocialMedia/SocialMedia';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+    <div className='white-gradient'/>
+
+     <Header/>
+     <Hero/>
+    
+    
+     <Compaines/>
+     <Politician/>
+     <Value/>
+     <Contact/>
+     <SocialMedia/>
+     <GetStarted/>
+     <br/>
+     <Footer/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
